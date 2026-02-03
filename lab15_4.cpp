834a77f7257c037de532e960efd411aa4453340c
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2026-02-03 13:34:41 +0700

    lab15_4.cpp

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,19 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a, int &b, int &c, int &d) {
+    // นำ Reference ของทั้ง 4 ตัวแปรมาเก็บไว้ใน Array ของ Pointer 
+    // เพื่อให้ง่ายต่อการใช้ Loop ในการสลับตำแหน่ง
+    int *arr[] = {&a, &b, &c, &d};
+
+    // ใช้ Fisher-Yates Shuffle Algorithm ในการสุ่มสลับตำแหน่ง
+    for (int i = 3; i > 0; i--) {
+        // สุ่มหา Index ตัวที่จะเอามาสลับ (0 ถึง i)
+        int j = rand() % (i + 1);
+
+        // ทำการ Swap ค่าในตัวแปรที่ Pointer ชี้อยู่
+        int temp = *arr[i];
+        *arr[i] = *arr[j];
+        *arr[j] = temp;
+    }
+}
